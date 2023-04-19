db3faa4c5cabbd155425f291b2af12fb78b079cd
Arnav K <arnavkaushal09@gmail.com>
2023-04-19 16:17:10 +0530

    Fixed 404 page

--- serve.ts
+++ serve.ts
@@ -1,10 +1,21 @@
+// imports 
 import Server from "lume/core/server.ts";
+import notFound from "lume/middlewares/not_found.ts";
 
+// new server
 const server = new Server({
     port: 8000,
     root: `${Deno.cwd()}/_site`,
 });
 
+// custom 404 page
+server.use(notFound({
+  root: `${Deno.cwd()}/_site`,
+  page404: "/404.html",
+}));
+
+
+// start server
 server.start();
 
 console.log("Listening on http://localhost:8000");
